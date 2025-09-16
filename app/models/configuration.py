bd82e0082e27af8da0edfba09aa07d85ff184d11
Gila Wersing <gila.wersing@gmail.com>
2025-09-16 12:53:16 -0500

    added str to clientprovconfig

--- app/models/configuration.py
+++ app/models/configuration.py
@@ -73,4 +73,7 @@ class ClaimProviderConfiguration:
         self.use_attending_provider_npi = use_attending_provider_npi
         self.use_supervising_provider_npi = use_supervising_provider_npi
 
+    def __str__(self):
+        return f'ClaimProviderConfiguration:use_attending_provider_npi:{self.use_attending_provider_npi},use_supervising_provider_npi:{self.use_supervising_provider_npi}'
+
 
